770a2a17d71f40755c8f2c2f5543a715ac5f59a4
Sone <sthaya@gmail.com>
2020-05-09 12:32:54 -0700

    created chuck norris joke project, connected to api, styled card using matrial ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { Button, Card, CardContent, CardActions, CssBaseline, Container, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  card: {
+    marginBottom: 20
+  },
+  cardContent: {
+    padding: '24px'
+  }
+})
 
 function App() {
+  const [jokes, setJokes] = useState([]);
+  const [jokesToShow, setJokesToShow] = useState([])
+
+  const classes = useStyles();
+
+  useEffect(() => {
+    fetch('https://api.icndb.com/jokes')
+      .then(res => res.json())
+      .then(res => {
+        console.log(res);
+        setJokes(res.value);
+        setJokesToShow(res.value.slice(0, 10))
+      })
+      .catch((err) => console.log(err))
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CssBaseline />
+      <Container>
+        <Typography variant='h1' align='center'>
+          Chuck Norris Jokes
+        </Typography>
+        {jokesToShow.map(joke => (
+          <Card key={joke.id} className={classes.card}>
+            <CardContent className={classes.cardContent}>
+              <Typography>{joke.joke}</Typography>
+            </CardContent>
+            <CardActions>
+              <Button variant='contained' color='primary'>
+                Like
+              </Button>
+              <Button variant='contained' color='default'>
+                Unlike
+              </Button>
+            </CardActions>
+          </Card>
+        ))}
+      </Container>
     </div>
   );
 }
